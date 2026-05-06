8789152bae0696f22235d313275040226f5cae86
Guigui <l.pasquier@bdx.ecolescreatives.com>
2026-05-06 10:48:37 +0200

    projectile joueur
    le projectile du joueur disparais après une certaine distance

--- Side_Scroller/Assets/Script/CharaProjectile1.cs
+++ Side_Scroller/Assets/Script/CharaProjectile1.cs
@@ -7,19 +7,27 @@ public class CharaProjectile1 : MonoBehaviour
    [SerializeField] private Transform projectileSpawnPoint;
    [SerializeField] private GameObject projectilePrefab;
    [SerializeField] private float projectileSpeed = 10f;
-   [SerializeField] private float projectileLifetime = 5f;
+ //  [SerializeField] private float projectileLifetime = 5f;
    [SerializeField] private float ShootDelay = 0.5f;
-
+   [SerializeField] private float maxDistance = 5f;
     float inputX;
     // Update is called once per frame
     void Update()
     {
-        
-
         if (Input.GetKeyDown(KeyCode.F))
         {
             var projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
-            projectile.GetComponent<Rigidbody2D>().linearVelocity = projectileSpawnPoint.position  * projectileSpeed;
+
+            var rb = projectile.GetComponent<Rigidbody2D>();
+            rb.linearVelocity = projectileSpawnPoint.right * projectileSpeed;
+
+            Destroy(projectile,maxDistance / projectileSpeed);
         }
+
+      /*  if (Input.GetKeyDown(KeyCode.F))
+        {
+            var projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+            projectile.GetComponent<Rigidbody2D>().linearVelocity = projectileSpawnPoint.position  * projectileSpeed;
+        }*/
     }
 }
