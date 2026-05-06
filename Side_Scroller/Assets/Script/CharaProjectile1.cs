150d63cc9c944160c739df50b04c2a6d45cc57c7
Guigui <l.pasquier@bdx.ecolescreatives.com>
2026-05-06 11:57:13 +0200

    projectile du joueur
    le joueur peut tirer dans les deux directions sans problème

--- Side_Scroller/Assets/Script/CharaProjectile1.cs
+++ Side_Scroller/Assets/Script/CharaProjectile1.cs
@@ -10,24 +10,43 @@ public class CharaProjectile1 : MonoBehaviour
  //  [SerializeField] private float projectileLifetime = 5f;
    [SerializeField] private float ShootDelay = 0.5f;
    [SerializeField] private float maxDistance = 5f;
+
+    float lastDirection = 1f;
     float inputX;
     // Update is called once per frame
     void Update()
     {
+
+        float inputX = Input.GetAxisRaw("Horizontal");
+
+        if (inputX != 0)
+        {
+            lastDirection = inputX;
+        }
+
+
         if (Input.GetKeyDown(KeyCode.F))
         {
-            var projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+            var projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, Quaternion.identity);
 
             var rb = projectile.GetComponent<Rigidbody2D>();
-            rb.linearVelocity = projectileSpawnPoint.right * projectileSpeed;
+            rb.linearVelocity = new Vector2(lastDirection* projectileSpeed,0f);
+
+
+            Collider2D playerCollider = GetComponent<Collider2D>();
+            Collider2D projectileCollider = projectile.GetComponent<Collider2D>();
+
+            Physics2D.IgnoreCollision(playerCollider, projectileCollider);
 
             Destroy(projectile,maxDistance / projectileSpeed);
         }
 
-      /*  if (Input.GetKeyDown(KeyCode.F))
-        {
-            var projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
-            projectile.GetComponent<Rigidbody2D>().linearVelocity = projectileSpawnPoint.position  * projectileSpeed;
-        }*/
+        /*  if (Input.GetKeyDown(KeyCode.F))
+          {
+              var projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+              projectile.GetComponent<Rigidbody2D>().linearVelocity = projectileSpawnPoint.position  * projectileSpeed;
+          }*/
+
+
     }
 }
