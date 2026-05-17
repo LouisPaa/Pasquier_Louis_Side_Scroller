7b1961912ebc5844008ebbd4c0e6c12d105cbcc1
PasquierLouis <l.pasquier@bdx.ecolescreatives.com>
2026-05-17 15:51:52 +0200

    Avancé
    je sais pas trop à quoi ca correspond

--- Side_Scroller/Assets/Script/Patrol.cs
+++ Side_Scroller/Assets/Script/Patrol.cs
@@ -6,11 +6,15 @@ public class Patrol : MonoBehaviour
     public Transform PatrolA;
     public Transform PatrolB;
 
+    [Header("Animation")]
+    private Animator Animation;
+    private string Marche = "Marche";
+
     public float dir = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Animation = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,7 +29,10 @@ public class Patrol : MonoBehaviour
         else if (Vector2.Distance(chara.position, PatrolB.position) < 0.1f)
         {
             dir = -1f;
+            transform.localScale = new Vector3(-1, 1, 1);
         }
+
+        Animation.SetBool(Marche, true);
 
         chara.position += new Vector3(1 * Time.deltaTime * dir, 0, 0);
     }
